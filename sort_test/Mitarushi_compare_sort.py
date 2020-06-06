102cb1797b424cee7e23d8afed911f65e4ee34e2
Mitarushi <takutakujaxa2@gmail.com>
2020-06-06 23:42:22 +0900

    compare sorts

--- sort_test/Mitarushi_compare_sort.py
+++ sort_test/Mitarushi_compare_sort.py
@@ -3,25 +3,53 @@ from random import randint
 import matplotlib.pyplot as plt
 from tqdm import trange
 
-MAX_LENGTH = 1000
+MAX_LENGTH = 4000
 MIN_LENGTH = 10
-TRY = 100000
+TRY = 10000
+sorts = [("Bucket Sort", swap_sort, "blue"),
+         ("Quick Sort", quick_sort, "green"),
+         ("Comb Sort", comb_sort, "red"),
+         ("Shell Sort", shell_sort, "cyan"),
+         ("Heap Sort", heap_sort, "magenta")]
+reverse_imfo = ("Inversion Number", get_reverse_count, "yellow")
 
 if __name__ == "__main__":
-    result_sort = []
+    result_n = []
+    result_sort = [[] for _ in sorts]
     result_reverse = []
+
     for _ in trange(TRY):
         n = randint(MIN_LENGTH, MAX_LENGTH)
         s = generate_random(n)
-        result_sort.append(quick_sort(s[:]))
+        result_n.append(n)
         result_reverse.append(get_reverse_count(s))
 
-    plt.figure(figsize=(12, 6))
-    plt.xlabel("Sort Cost")
-    plt.ylabel("Inversion Number")
-    plt.grid()
-    plt.xlim(0, 600000)
-    plt.ylim(0, 300000)
-    plt.axes().set_aspect("equal", "datalim")
-    plt.scatter(result_sort, result_reverse, s=2)
-    plt.savefig("sort_test\\result_quick.png")
+        for i, (_, func, _) in enumerate(sorts):
+            result_sort[i].append(func(s[:]))
+
+    plt.figure(figsize=(14, 12))
+    ax1 = plt.subplot2grid((2, 7), (0, 0), 2, 3)
+    ax2 = plt.subplot2grid((2, 7), (0, 4), 2, 3)
+
+    ax1.set_xlabel("Length")
+    ax1.set_ylabel("Sort Cost")
+    ax1.set_xlim(0, MAX_LENGTH)
+    ax1.set_ylim(0, 16000000)
+    ax1.grid()
+    for i, (name, _, color) in enumerate(sorts):
+        ax1.scatter(result_n, result_sort[i], label=name, c=color, s=1)
+    ax1.scatter(result_n, result_reverse,
+                label=reverse_imfo[0], c=reverse_imfo[2], s=1)
+    ax1.legend()
+
+    ax2.set_xlabel("Inversion Number")
+    ax2.set_ylabel("Sort Cost")
+    ax2.set_xlim(0, 4000000)
+    ax2.set_ylim(0, 8000000)
+    ax2.grid()
+    ax2.set_aspect("equal", "datalim")
+    for i, (name, _, color) in enumerate(sorts):
+        ax2.scatter(result_reverse, result_sort[i], label=name, c=color, s=1)
+    ax2.legend()
+
+    plt.savefig("sort_test\\result_all.png")
--- sort_test/Mitarushi_sort_reverse.py
+++ sort_test/Mitarushi_sort_reverse.py
@@ -23,9 +23,9 @@ def swap_sort(s):
         else:
             j = char_from[s[i]]
             char_from[s[i]] += 1
-            cost += abs(j - i)
-            # print(j, i)
-            s[j], s[i] = s[i], s[j]
+            if s[i] != s[j]:
+                cost += abs(j - i)
+                s[j], s[i] = s[i], s[j]
             is_sorted[j] = True
     return cost
 
@@ -52,6 +52,80 @@ def quick_sort(s, l=0, r=-1):
             j -= 1
         cost += quick_sort(s, l, i - 1)
         cost += quick_sort(s, j + 1, r)
+    return cost
+
+
+def comb_sort(s):
+    h = len(s) * 10 // 13
+    cost = 0
+    while True:
+        is_swaped = False
+        for i in range(len(s) - h):
+            if s[i] > s[i + h]:
+                s[i], s[i + h] = s[i + h], s[i]
+                cost += h
+                is_swaped = True
+        if h == 1 and (not is_swaped):
+            return cost
+        h = max(1, h * 10 // 13)
+
+
+def shell_sort(s):
+    n = len(s)
+    h = 1
+    while h * 3 + 1 < n:
+        h = h * 3 + 1
+
+    cost = 0
+    while True:
+        for i in range(h):
+            for j in range(i, n, h):
+                while j >= h and s[j - h] > s[j]:
+                    s[j - h], s[j] = s[j], s[j - h]
+                    j -= h
+                    cost += h
+        if h == 1:
+            return cost
+        else:
+            h = (h - 1) // 3
+
+
+def heap_sort(s):
+    n = len(s)
+    cost = 0
+    for i in range(n):
+        while i > 0:
+            j = (i - 1) // 2
+            if s[j] < s[i]:
+                s[i], s[j] = s[j], s[i]
+                cost += abs(j - i)
+            else:
+                break
+            i = j
+
+    for i in reversed(range(n)):
+        if s[i] != s[0]:
+            s[0], s[i] = s[i], s[0]
+            cost += i
+
+        j = 0
+        while True:
+            l = j * 2 + 1
+            r = j * 2 + 2
+            if l >= i:
+                break
+
+            tmp = j
+            if r < i and s[r] > s[j]:
+                tmp = r
+            if s[l] > s[j] and s[l] > s[r]:
+                tmp = l
+
+            if tmp == j:
+                break
+            cost += abs(tmp - j)
+            s[tmp], s[j] = s[j], s[tmp]
+            j = tmp
     return cost
 
 
@@ -93,6 +167,9 @@ if __name__ == "__main__":
     s = generate_random(100)
     #s = [4, 3, 5, 2, 1]
     print(sorted(s))
-    print(swap_sort(s[:]))
-    print(quick_sort(s[:]))
+    print("bucket", swap_sort(s[:]))
+    print("quick", quick_sort(s[:]))
+    print("comb", comb_sort(s[:]))
+    print("shell", shell_sort(s[:]))
+    print("heap", heap_sort(s[:]))
     print(get_reverse_count(s))
